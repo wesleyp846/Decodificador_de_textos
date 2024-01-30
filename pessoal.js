3478c7f0e0afd94a784c3a0cd31a043dae342469
Wesley Pereira <aventureiro-msn@hotmail.com>
2024-01-29 23:41:31 -0300

    update

--- pessoal.js
+++ pessoal.js
@@ -1,15 +1,59 @@
-const themeToggle = document.getElementById('themeToggle');
-const body = document.body;
+//Esse TexTo TeM MuiTAs LETRAS EMBARALHADAS 
 
-themeToggle.addEventListener('change', () => {
-    body.classList.toggle('dark-theme');
-  });
+// function criptografar(){
+//   crip = criptografia(pegaTextoI());
+//   addCrip(crip);
+//   exibenatela();
+// }
 
-/*
-const themeToggle = document.getElementById('themeToggle');
-const body = document.body;
+// function addCrip(crip){
+//   let surge = document.getElementById("apareci");
+//   surge.innerHTML = crip;
+// }
 
-themeToggle.addEventListener('change', () => {
-  body.classList.toggle('dark-theme');
-});
-*/
+
+function criptografar(texto){
+  let crip = criptografia(pegaTexto(texto));
+  addTexto(crip);
+  console.log(crip, "\n\nfun criptografar var crip");
+}
+
+function addTexto(crip){
+  document.getElementById('output-texto').value = crip;
+}
+
+function pegaTexto(idtexto){
+  let texto = document.getElementById(idtexto).value;
+  texto = tansformaTexto(texto);
+  limpaTexto(idtexto);
+  return texto;
+}
+
+function tansformaTexto(texto){
+  texto = texto.trim().toLowerCase();
+  return texto;
+}
+
+function limpaTexto(idtexto){
+  document.getElementById(idtexto).value = "";
+}
+
+function criptografia(texto){
+  let mensagem = texto.
+      replaceAll("e", "enter").
+      replaceAll("i", "imes").
+      replaceAll("a", "ai").
+      replaceAll("o", "ober").
+      replaceAll("u", "ufat");
+  return mensagem;
+}
+
+function descriptografia(texto){
+  let mensagem = texto.
+  replaceAll("enter", "e").
+  replaceAll("imes", "i").
+  replaceAll("ai", "a").
+  replaceAll("ober", "o").
+  replaceAll("ufat", "u");
+  return mensagem;
+}
